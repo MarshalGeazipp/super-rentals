8fed91b9e9306fefd258fb943623eefdfaf65cc3
Sebastian Nicklas <marshal.geazipp@gmail.com>
2020-04-05 15:39:11 +0200

    Completed chapter „Reusable Components“

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function(environment) {
+module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'super-rentals',
     environment,
@@ -46,6 +46,8 @@ module.exports = function(environment) {
   if (environment === 'production') {
     // here you can enable a production-specific feature
   }
+
+  ENV.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibWFyc2hhbGdlYXppcHAiLCJhIjoiY2s4bjJiOGEyMGlvbjNlb2NlODY1emd3dCJ9.IBRgK1yfLxhbb2z0MPlVJw';
 
   return ENV;
 };
